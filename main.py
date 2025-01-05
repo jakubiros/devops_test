1442d0ac504c7dd3e8a86248ba5fe89ca8d2563e
jakubiros <116426972+jakubiros@users.noreply.github.com>
2025-01-05 20:30:23 +0100

    main fix

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def sum(x,y):
-    return x+y
+from flask import Flask
 
-print(sum(5,2))
+app = Flask(__name__)
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello, %s!' % name
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000,debug=True)
