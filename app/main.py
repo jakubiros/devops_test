70d77918fcdd7787bc4eacf576587498d9700f58
jakubiros <116426972+jakubiros@users.noreply.github.com>
2025-01-06 19:07:42 +0100

    server_time add

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#.
+from datetime import datetime
 
 app = Flask(__name__)
 @app.route('/hello/<name>')
@@ -9,6 +9,11 @@ def hello_name(name):
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/time')
+def server_time():
+    now = datetime.now()
+    return f"Current server time is: {now.strftime('%Y-%m-%d %H:%M:%S')}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)
